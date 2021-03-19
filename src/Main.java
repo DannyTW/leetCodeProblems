f635f338cb001e8aaff37699df337e3a7e9772f9
khanonfodder <dannythewireless+github@gmail.com>
2021-03-18 21:23:15 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /** two-sum
+        try {
+            int[] nums = {2, 5, 5, 11};
+            int target = 1;
+
+            int[] result = Twosum.twoSum(nums, target);
+            for (int number : result) {
+                System.out.println(number);
+            }
+        } catch (Exception exc) {
+            System.out.println("An exception was thrown:" + exc);
+        }
+        **/
+    }
+}
---
+++ src/TwosumTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwosumTest {
+
+    @Test
+    public void twoSum() {
+    }
+}
